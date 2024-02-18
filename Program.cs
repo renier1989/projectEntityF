06a553a82baadeaa84e88d5cac423fe2db8cd870
Renier Vargas <renier1989@gmail.com>
2024-02-17 19:58:27 -0500

    creacion de la BD en MySql con entity Framework

--- Program.cs
+++ Program.cs
@@ -4,19 +4,23 @@ using projectEF;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// // configuracion general de entity framework en el proyecto para probar la configuracion con un BD en memoria
+// builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
 
-// configuracion general de entity framework en el proyecto
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+// esto es para la conexion con mysql, aqui uso el puerto 3333 porque es el que esta desplegado en docker el puerto por defecto es el 3306
+builder.Services.AddDbContext<TareasContext>(p => p.UseMySQL("Server=localhost;Database=TareasDB;Port=3333;User=root;Password=root;"));
+
+// string connStr = "server=localhost;user=root;database=world;password=root";
+// MySqlConnection conn = new MySqlConnection(connStr);
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// esto es para verificar que la configuracion de los modelos y entity framework funcionan correctamente
 app.MapGet("/dbconexion", async([FromServicesAttribute] TareasContext dbContext)=>{
     dbContext.Database.EnsureCreated();
     return Results.Ok($"Base de Datos Creado en Memoria: {dbContext.Database.IsInMemory()} ");
 });
 
 app.Run();
-
-
--- obj/Debug/net8.0/projectEF.AssemblyInfo.cs
+++ obj/Debug/net8.0/projectEF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectEF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62f0811566599fd52af8aeb6620d430ddc66a47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9f03e826607e76cbf1eb70a44cd400c4047ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectEF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectEF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
