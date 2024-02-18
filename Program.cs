1d9f03e826607e76cbf1eb70a44cd400c4047ba1
Renier Vargas <renier1989@gmail.com>
2024-02-17 19:27:37 -0500

    creando un BD en memoria para verificar la configuracion con entity framework entre el contexto y los modelos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectEF;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// configuracion general de entity framework en el proyecto
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async([FromServicesAttribute] TareasContext dbContext)=>{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok($"Base de Datos Creado en Memoria: {dbContext.Database.IsInMemory()} ");
+});
 
 app.Run();
 
